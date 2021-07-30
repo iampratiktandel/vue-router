45a1b315cc35a0e396a7181157dd5ceeb1dacf3b
iampratiktandel <iampratiktandel@gmail.com>
2021-07-30 23:58:02 +0530

    programmatic navigation

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,31 @@
     <router-link :to="{name: 'About'}">About</router-link> |
     <router-link :to="{name: 'Jobs'}">Jobs</router-link>
   </div>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="goBack">Go Back</button>
+  <button @click="goForward">Go Forward</button>
+
   <router-view/>
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'Home' })
+    },
+
+    goBack() {
+      this.$router.go(-1)
+    },
+
+    goForward() {
+      this.$router.go(1)
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -29,5 +52,12 @@
 #nav a.router-link-exact-active {
   color: #42b983;
   background: red;
+}
+
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 10px;
 }
 </style>
